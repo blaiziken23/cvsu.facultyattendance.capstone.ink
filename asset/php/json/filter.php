3399670deb507766c0c2b8c3ef6faca087c6ced3
Jayvie <jayviee.berdin@gmail.com>
2023-11-29 11:25:43 +0800

    Update filter.php

--- asset/php/json/filter.php
+++ asset/php/json/filter.php
@@ -15,67 +15,57 @@
     $from = $_GET["from"];
     $to = $_GET["to"];
 
-    // $filter = "SELECT 
-    //             employee_information.firstname AS fname, 
-    //             employee_information.lastname AS lname,
-    //             employee_attendance.date AS dates, 
-    //             employee_information.employee_id AS empid, 
-    //             employee_attendance.time_in AS timein,
-    //             employee_attendance.time_out AS timeout,
-    //             employee_attendance.total_hour AS tot_hour,
-    //             employee_attendance.status AS status
-    //           FROM 
-    //             employee_attendance
-    //           INNER JOIN 
-    //             employee_information
-    //           ON 
-    //             employee_information.information_id = employee_attendance.information_id
-    //           WHERE 
-    //             employee_attendance.date 
-    //               BETWEEN '$from' AND '$to' 
-    //             AND (employee_information.firstname LIKE '%$id_or_name%' 
-    //             OR employee_information.lastname LIKE '%$id_or_name%'
-    //             OR employee_information.employee_id = '$id_or_name')";
-
-
-    
-    $filter = "SELECT `id`, `date`, `time_in`, `time_out`, `status`, `total_hour` FROM `attendance_tbl` WHERE `information_id` = '$id' AND `date` BETWEEN '$from' AND '$to' ORDER BY `id` DESC";
+    $filter = "SELECT `id`, `fullname`, `date`, `time_in`, `time_out`, `status`, `total_hour` FROM `attendance_tbl` WHERE `information_id` = '$id' AND `date` BETWEEN '$from' AND '$to' ORDER BY `date`";
     $result = mysqli_query($conn, $filter);
-  
+    $row = mysqli_fetch_assoc($result);
+    $name = $row["fullname"];
     echo '<table class="table" id="sample">
           <tr>
             <th rowspan="2" class="align-middle text-center">Day</th>
             <th colspan="2" class="align-middle text-center">AM</th>
             <th colspan="2" class="align-middle text-center">PM</th>
+            <th colspan="2" class="align-middle text-center">Undertime</th>
           </tr>
           <tr>
               <th>Arrival</th>
               <th>Departure</th>
               <th>Arrival</th>
               <th>Departure</th>
+              <th>Hours</th>
+              <th>Minutes</th>
           </tr>';
 
   while ($row = mysqli_fetch_assoc($result)) {
-      echo '<tr>';
-      echo '<td>' . $row['date'] . '</td>';
+    echo '<tr>';
+    echo '<td>' . $row['date'] . '</td>';
+    
+    echo $row['total_hour'] . " " ;
 
-      if (strtotime($row['time_in']) < strtotime('12:00:00')) {
-          echo '<td>' . date("h:i", strtotime($row['time_in']))  . '</td>';
-          echo '<td>' . date("h:i", strtotime($row['time_out'])) . '</td>';
-          echo '<td></td>';
-          echo '<td></td>';
-      } 
-      else {
-          echo '<td></td>';
-          echo '<td></td>';
-          echo '<td>' . date("h:i", strtotime($row['time_in'])) . '</td>';
-          echo '<td>' . date("h:i", strtotime($row['time_out'])) . '</td>';
-      }
+    if (strtotime($row['time_in']) < strtotime('12:00:00')) {
+      echo '<td>' .  date("h:i A", strtotime($row['time_in']))  . '</td>';
+    }
+    if (strtotime($row['time_in']) < strtotime('12:00:00') && strtotime($row['time_out']) > strtotime('12:00:00') || $row['time_out'] == "12:00:00") {
+      echo '<td></td>';
+      echo '<td></td>';
+      echo '<td>' .  date("h:i A", strtotime($row['time_out']))  . '</td>';
+    }
+    if (strtotime($row['time_in']) < strtotime('12:00:00') && strtotime($row['time_out']) < strtotime('12:00:00')) {
+      echo '<td>' .  date("h:i A", strtotime($row['time_out']))  . '</td>';
+    }
 
-      echo '</tr>';
+    if (strtotime($row['time_in']) > strtotime('12:00:00')) {
+      echo '<td></td>';
+      echo '<td></td>';
+      echo '<td>' .  date("h:i A", strtotime($row['time_in']))  . '</td>';
+    }
+    if (strtotime($row['time_in']) > strtotime('12:00:00') && strtotime($row['time_out']) > strtotime('12:00:00')) {
+      echo '<td>' .  date("h:i A", strtotime($row['time_out']))  . '</td>';
+    }
+
+    
+
+    echo '</tr>';
   }
-
-  // Tapusin ang HTML table
   echo '</table>';
   }
 
@@ -168,7 +158,6 @@
 <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <script>
-  console.log("hello1");
   window.jsPDF = window.jspdf.jsPDF;
 
   const doc = new jsPDF()
@@ -192,22 +181,37 @@
     XLSX.writeFile(file, 'file.' + type);
   }
 
+
+
+  
   document.getElementById('export').addEventListener('click', () =>  {
     //html_table_to_excel('xlsx');
     console.log("hello");
 
     doc.setFontSize(8)
-    doc.setFont('', 'italic', 'normal');
+    doc.setFont('Helvetica', 'italic', '');
     doc.text('Civil Service Form No. 48', 25, 20)
     doc.setFontSize(13)
-    doc.setFont('', '', 800);
-    doc.text('DAILY TIME RECORD', 40, 28)
+    doc.setFont('Helvetica', '', 'Bold');
+    doc.text('DAILY TIME RECORD', 150/2, 28, {
+      align: 'center'
+    })
     doc.setFontSize(12)
-    doc.text('BERDIN JAYVIE', 30, 39)
-    doc.setLineWidth(.3); 
+    doc.text('<?php echo $name; ?>', 150/2, 39, {align: 'center'})
     doc.line(25, 40, 125, 40);
     doc.setFontSize(8)
-    doc.text('(Name)', 65, 43)
+    doc.setFont('Helvetica', '', 'normal');
+    doc.text('(Name)', 150/2, 43, {align: 'center'})
+    doc.setFont('Helvetica', 'italic', 'normal');
+    doc.text('For the month of', 25, 50)
+    doc.line(50, 52, 125, 52);
+    doc.text("Official hours for arrival", 25, 59)
+    doc.text("and departure", 30, 62)
+    doc.text("Regular days", 70, 57)
+    doc.line(90, 59, 125, 59);
+    doc.text("Saturdays", 74, 63)
+    doc.line(90, 65, 125, 65);
+
     doc.autoTable({ 
       html: '#sample', 
       columnStyles: { 
@@ -230,17 +234,27 @@
         4: {
           halign: 'center',
           cellWidth: 20
+        },
+        5: {
+          halign: 'center',
+          cellWidth: 20
+        },
+        6: {
+          halign: 'center',
+          cellWidth: 20
         }
       },
       theme: 'grid',
       margin: {
-        top: 60,
+        top: 70,
         left: 25
       },
       styles: {
         fontSize: 8
       },
-      tableWidth: 80
+      alternateRowStyles: {
+        //valign: 'center'
+      }
     });
     doc.save('table.pdf')
   });
